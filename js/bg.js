e67fe92da9839daba5800eae04df277c3720706f
vangona <rlarhksrud12@naver.com>
2021-05-23 16:34:13 +0900

    2021.05.23 init commit

--- js/bg.js
+++ js/bg.js
@@ -1,16 +1,38 @@
 const body = document.querySelector("body")
 
-const IMG_NUMBER = 13;
+const userAgent = navigator.userAgent;
+
+const IMG_NUMBER_LND = 3;
+const IMG_NUMBER_POR = 2;
+
+let number = 1;
+
+function isMobile(){
+	if (userAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || userAgent.match(/LG|SAMSUNG|Samsung/) != null){
+		return true;
+    } else{
+		return false;
+	}
+}
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `./images/${imgNumber + 5}.jpg`;
+    if(isMobile()) {
+        image.src = `./images/por/${imgNumber}.jpg`;
+    }
+    else {
+        image.src = `./images/lan/${imgNumber}.jpg`;
+    }
     image.classList.add("bgImage");
     body.prepend(image);
 }
 
 function genRandom(){
-    const number = Math.floor(Math.random() * IMG_NUMBER);
+    if(isMobile()) {
+        number = 1 + Math.floor(Math.random() * IMG_NUMBER_POR);
+    } else {
+        number = 1 + Math.floor(Math.random() * IMG_NUMBER_LND);
+    }
     return number;
 }
 
